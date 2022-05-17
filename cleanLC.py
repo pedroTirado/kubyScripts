c544b48cb77391ee90a767829ec952e7c7a8d598
pedroTirado <17729184+pedroTirado@users.noreply.github.com>
2022-05-17 00:07:40 -0400

    need to sleep on this...

---
+++ cleanLC.py
@@ -0,0 +1,23 @@
+
+TOPIC_COUNT = 6 # change according to input file(s)
+topics = []
+
+"""
+initialize dict for each TOPIC
+"""
+# def initDicts()
+
+"""
+initialize double arraylist for TOPIC
+"""
+def initLists(topic: dict, subCount: int):
+    
+    for i in range(0, subCount): # 0 to listCount-1
+        subtopic = []
+
+if __name__ == "__main__":
+
+    # dict for each TOPIC
+    for i in range(0, TOPIC_COUNT): # ...
+        topics.append({})
+        
